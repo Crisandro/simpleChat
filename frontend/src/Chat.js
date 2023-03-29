02b8dda19ed3afe7e4d5832599fe655a63886934
Crisandro Basoy <35168198+Crisandro@users.noreply.github.com>
2023-03-30 06:56:45 +0800

    Update Chat.js

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,12 +1,14 @@
 import React,{useState} from "react"
-import {Box, Divider, Paper} from '@mui/material/'
+import {Box, Divider, Paper, Backdrop, CircularProgress} from '@mui/material/'
 import ContactList from "./ContactList"
 import ChatInfo from "./ChatInfo"
 
 export default function Chat(){
     const [ selectedUser, setSelectedUser ] = useState([])
+    const [open, setOpen] = useState(false);
 
     const handleSelectUser = (userData) => {
+        setOpen(true)
         setSelectedUser(userData)
     }
 
@@ -14,6 +16,13 @@ export default function Chat(){
         <Box
             sx={{ width: '100%', height: '95vh', display: 'flex', gap: '2px'}}
         >
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+
             <ContactList selectUser={handleSelectUser} />
             <Divider orientation="vertical" flexItem />
             {selectedUser.length === 0 ? (
@@ -27,7 +36,7 @@ export default function Chat(){
                     Search users to start chatting .
                 </Paper>
             ): (  
-                <ChatInfo userDetails={selectedUser}/>
+                <ChatInfo userDetails={selectedUser} setOpen={setOpen}/>
             )}
         </Box>
     )
